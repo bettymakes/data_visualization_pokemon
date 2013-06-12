f789ba93bbe896899f6877dfe8d6ac3727d8ab4c
blibotics <blibotics@gmail.com>
2013-06-12 18:26:44 -0400

    Deleted unneccessary comments from file

--- data-visualization.rb
+++ data-visualization.rb
@@ -8,8 +8,6 @@ url = "http://pokemondb.net/pokedex/national"
 # Nokogiri object, parse HTML document (nokogiri can parse diff types of docs), open this 'url' (line 6)
 data = Nokogiri::HTML(open(url))
 
-# puts data
-
 pokemon = data.css(".item")
 
 pokemon.each do |info|
@@ -18,22 +16,3 @@ pokemon.each do |info|
 	poke_type = info.at_css(".note")
 		puts "Type: " + poke_type.text.strip
 end
-
-
-
-
-# require 'nokogiri'
-# require 'open-uri'
-
-# url = "http://toronto.en.craigslist.ca/apa/"
-# data = Nokogiri::HTML(open(url))
-# listings = data.css(".row")
-# listings.each do |item|
- 
-
-# description = item.at_css(".pl")
-# if !description.nil?
-#   puts "Description: " + description.text.strip
-# else
-#   puts "Description: N/A"
-# end
