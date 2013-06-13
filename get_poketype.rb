d2760e0834cfe19f605c4b664571b6340e590b25
blibotics <blibotics@gmail.com>
2013-06-13 15:04:01 -0400

    Created arrays for all poketypes

--- get_poketype.rb
+++ get_poketype.rb
@@ -16,7 +16,25 @@ require 'nokogiri'
 	fire_poketype_array = Array.new
 	grass_poketype_array = Array.new
 	rock_poketype_array = Array.new
-	
+	water_poketype_array = Array.new
+	bug_poketype_array = Array.new
+	normal_poketype_array = Array.new
+	poison_poketype_array = Array.new
+	electric_poketype_array = Array.new
+	ground_poketype_array = Array.new
+	fighting_poketype_array = Array.new
+	psychic_poketype_array = Array.new
+	ghost_poketype_array = Array.new
+	ice_poketype_array = Array.new
+	dragon_poketype_array = Array.new
+	steel_poketype_array = Array.new
+	dark_poketype_array = Array.new
+	# rock_poketype_array = Array.new
+	# rock_poketype_array = Array.new
+	# rock_poketype_array = Array.new
+
+
+
 	pokemon.each do |info|
 		poke_type = info.at_css(".note").text.strip.gsub(/(\W.+)/, "")
 		poketype_array << poke_type
@@ -26,6 +44,20 @@ require 'nokogiri'
 			fire_poketype_array << type  if type == "Fire"
 			grass_poketype_array << type  if type == "Grass"
 			rock_poketype_array << type  if type == "Rock"
+			water_poketype_array << type if type == "Water"
+			bug_poketype_array << type if type == "Bug"
+			normal_poketype_array << type if type == "Normal"
+			poison_poketype_array << type if type == "Poison"
+			electric_poketype_array << type if type == "Electric"
+			ground_poketype_array << type if type == "Ground"
+			fighting_poketype_array << type if type == "Fighting"
+			psychic_poketype_array << type if type == "Psychic"
+			ghost_poketype_array << type if type == "Ghost"
+			ice_poketype_array << type if type == "Ice"
+			dragon_poketype_array << type if type == "Dragon"
+			steel_poketype_array << type if type == "Steel"
+			dark_poketype_array << type if type == "Dark"
+
 				
 		end
 
