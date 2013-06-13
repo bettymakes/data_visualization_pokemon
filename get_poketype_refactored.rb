d658a45c80dbbb405190828a76a44911f5029f89
blibotics <blibotics@gmail.com>
2013-06-13 18:11:21 -0400

    Refactored code for updating values in hash with # of types.

--- get_poketype_refactored.rb
+++ get_poketype_refactored.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 	
 	pokemon = data.css(".item")
 
-	poke_categories = %w(fire grass rock water bug normal poison electic ground fighting 
+	poke_categories = %w(fire grass rock water bug normal poison electric ground fighting 
 									psychic ghost ice dragon steel dark flying)
 
 	poke_cat_hash = {}
@@ -12,7 +12,7 @@ require 'nokogiri'
 		poke_cat_hash[ cat.to_sym ] = []
 	end
 
-	# poketype_array = Array.new
+	poketype_array = Array.new
 	# cat_values_array = Array.new
 
 	pokemon.each do |info|
@@ -22,40 +22,34 @@ require 'nokogiri'
 
 		poketype_array.each do |type|
 			# data_hash[:fire] << type if type == "Fire"
-			data_hash[ type.downcase.to_sym ] << type
+			poke_cat_hash[ type.downcase.to_sym ] << type
 		end
 
+		poke_cat_hash.each_pair do |key, value|
+			# count the number of words in the values array = "counted_values"
+			# replace array of values with "counted_values" 
+			# we'll need to refer the "counted_values" back to the key in the hash
+
+			# poke_cat_hash[key] = [value_array.length]
+			poke_cat_hash[key] = value.length
+			end
+
+		p poke_cat_hash
+
 count_total = poketype_array.length
-cat_values_array <<		count_fire 			= fire_poketype_array.length
-cat_values_array <<		count_grass 		= grass_poketype_array.length
-cat_values_array <<		count_rock 			= rock_poketype_array.length
-cat_values_array <<		count_water 		= water_poketype_array.length
-cat_values_array <<		count_bug 			= bug_poketype_array.length
-cat_values_array <<		count_normal 		= normal_poketype_array.length
-cat_values_array <<		count_poison 		= poison_poketype_array.length
-cat_values_array <<		count_electric 	= electric_poketype_array.length
-cat_values_array <<		count_ground 		= ground_poketype_array.length
-cat_values_array <<		count_fighting 	= fighting_poketype_array.length
-cat_values_array <<		count_pychic 		= psychic_poketype_array.length
-cat_values_array <<		count_ghost 		= ghost_poketype_array.length
-cat_values_array <<		count_ice 			= ice_poketype_array.length
-cat_values_array <<		count_dragon 		= dragon_poketype_array.length
-cat_values_array <<		count_steel 		= steel_poketype_array.length
-cat_values_array <<		count_dark 			= dark_poketype_array.length
-cat_values_array <<		count_flying 		= flying_poketype_array.length
 
 
-File.open('poke_page.html', 'w') do |html|
-	for i in 0..16 do
-	div_width = (count_type_array / count_total)*100
+# File.open('poke_page.html', 'w') do |html|
+# 	for i in 0..16 do
+# 	div_width = (count_type_array / count_total)*100
 
-	html.write("<div class="bar-default">")
-	html.write("	<div class='bar-filled highlight' style='width:' + div_width + '%;' >\n")
-								#<div class="bar-filled highlight" style="width: 100%"></div>
-	html.write("<div class="bar-title">___variable____</div>")
-	html.write("</div>")
-	html.write("</div>")
-end
+# 	html.write("<div class="bar-default">")
+# 	html.write("	<div class='bar-filled highlight' style='width:' + div_width + '%;' >\n")
+# 								#<div class="bar-filled highlight" style="width: 100%"></div>
+# 	html.write("<div class="bar-title">___variable____</div>")
+# 	html.write("</div>")
+# 	html.write("</div>")
+# end
 
 
 
