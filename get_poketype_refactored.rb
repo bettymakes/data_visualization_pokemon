16410a3e87b22d2c10fab185c0dc24ae89e0a171
blibotics <blibotics@gmail.com>
2013-06-13 20:49:07 -0400

    Finished refactoring code and created html file

--- get_poketype_refactored.rb
+++ get_poketype_refactored.rb
@@ -1,7 +1,8 @@
 require 'nokogiri'
 	file = 'pokemon-list.html'  #downloaded site to file, scraping downloaded file instead
 	data = Nokogiri::HTML(open( file ))
-	
+	File.open('poke_page.html', 'w') do |html|
+
 	pokemon = data.css(".item")
 
 	poke_categories = %w(fire grass rock water bug normal poison electric ground fighting 
@@ -19,6 +20,7 @@ require 'nokogiri'
 		poke_info = info.at_css(".note").text.strip.gsub(/(\W.+)/, "")
 		poketype_array << poke_info
 	end
+	poketype_total = poketype_array.length
 
 		poketype_array.each do |type|
 			# data_hash[:fire] << type if type == "Fire"
@@ -27,30 +29,33 @@ require 'nokogiri'
 
 		poke_cat_hash.each_pair do |key, value|
 			# count the number of words in the values array = "counted_values"
-			# replace array of values with "counted_values" 
+			# replace  arrayof values with "counted_values" 
 			# we'll need to refer the "counted_values" back to the key in the hash
 
 			# poke_cat_hash[key] = [value_array.length]
-			poke_cat_hash[key] = value.length
+			poke_cat_hash[key]  = value.length
 			end
 
-		p poke_cat_hash
+# p ((poke_cat_hash[:fire].to_f/poketype_total.to_f)*100).to_i
+# p poke_cat_hash[:fire]
+# p poke_cat_hash.values
+# p poke_cat_hash.keys.to_s.upcase
 
-count_total = poketype_array.length
 
 
-# File.open('poke_page.html', 'w') do |html|
-# 	for i in 0..16 do
-# 	div_width = (count_type_array / count_total)*100
-
-# 	html.write("<div class="bar-default">")
-# 	html.write("	<div class='bar-filled highlight' style='width:' + div_width + '%;' >\n")
-# 								#<div class="bar-filled highlight" style="width: 100%"></div>
-# 	html.write("<div class="bar-title">___variable____</div>")
-# 	html.write("</div>")
-# 	html.write("</div>")
-# end
-
+			poke_cat_hash.each_pair do |key, value|
+			div_width = ((value.to_f / poketype_total.to_f)*100)
+			
+			p div_width
+			html.write("<div class='bar_default'>\n")
+			html.write("	<div class='bar_filled highlight' style='width: " + div_width.to_s + "%;'>\n")
+										#<div class="bar-filled highlight" style="width: 100%"></div>
+			html.write("	<div class='bar_title'>" + key.upcase.to_s + "</div>\n")
+			html.write("	</div>\n")
+			html.write("</div>\n")
+			html.write("\n")
+	end
+end
 
 
 
